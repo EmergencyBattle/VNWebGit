bdd1cf9d594d26ad4df2e185e85fb904c0c8db7b
Emilia <emergencybattle@gmail.com>
2018-01-24 20:56:11 +0100

    Added content
    Concept Studio projects and Cupid illustration

--- VNWebScriptProjects.js
+++ VNWebScriptProjects.js
@@ -8,6 +8,12 @@ var lumbermodal = document.getElementById("lumberModal");
 var mycellmodal = document.getElementById("mycellModal");
 var ppmodal = document.getElementById("ppModal");
 var awdmodal = document.getElementById("awdModal");
+var sambmodal = document.getElementById("sambModal");
+var avfallmodal = document.getElementById("avfallModal");
+var compmodal = document.getElementById("compModal");
+var kramforsmodal = document.getElementById("kramforsModal");
+var sprakmodal = document.getElementById("sprakModal");
+var susannamodal = document.getElementById("susannaModal");
 
 
 // Get the button that opens the modal
@@ -20,6 +26,12 @@ var lumberthumb = document.getElementById("lumber");
 var mycellthumb = document.getElementById("mycell");
 var ppthumb = document.getElementById("pp");
 var awdthumb = document.getElementById("awd");
+var sambthumb = document.getElementById("samb");
+var avfallthumb = document.getElementById("avfall");
+var sprakthumb = document.getElementById("sprak");
+var kramforsthumb = document.getElementById("kramfors");
+var compthumb = document.getElementById("comp");
+var susannathumb = document.getElementById("susanna");
 
 // Get the <span> element that closes the modal
 //var thesisClose = document.getElementById("thesisclose");
@@ -40,6 +52,18 @@ var ppClose = document.getElementById("ppclose");
 var ppCtc = document.getElementById("ppctc");
 var awdClose = document.getElementById("awdclose");
 var awdCtc = document.getElementById("awdctc");
+var sprakClose = document.getElementById("sprakclose");
+var sprakCtc = document.getElementById("sprakctc");
+var compClose = document.getElementById("compclose");
+var compCtc = document.getElementById("compctc");
+var kramforsClose = document.getElementById("kramforsclose");
+var kramforsCtc = document.getElementById("kramforsctc");
+var sambClose = document.getElementById("sambclose");
+var sambCtc = document.getElementById("sambctc");
+var avfallClose = document.getElementById("avfallclose");
+var avfallCtc = document.getElementById("avfallctc");
+var susannaClose = document.getElementById("susannaclose");
+var susannaCtc = document.getElementById("susannactc");
 
 // When the user clicks on the button, open the modal
 //thesisthumb.onclick = function () {
@@ -68,6 +92,25 @@ ppthumb.onclick = function () {
 }
 awdthumb.onclick = function () {
     awdmodal.style.display = "block";
+}
+
+sprakthumb.onclick = function () {
+    sprakmodal.style.display = "block";
+}
+avfallthumb.onclick = function () {
+    avfallmodal.style.display = "block";
+}
+kramforsthumb.onclick = function () {
+    kramforsmodal.style.display = "block";
+}
+sambthumb.onclick = function () {
+    sambmodal.style.display = "block";
+}
+compthumb.onclick = function () {
+    compmodal.style.display = "block";
+}
+susannathumb.onclick = function () {
+    susannamodal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
@@ -126,6 +169,43 @@ awdCtc.onclick = function () {
     awdmodal.style.display = "none";
 }
 
+sprakClose.onclick = function () {
+    sprakmodal.style.display = "none";
+}
+sprakCtc.onclick = function () {
+    sprakmodal.style.display = "none";
+}
+kramforsClose.onclick = function () {
+    kramforsmodal.style.display = "none";
+}
+kramforsCtc.onclick = function () {
+    kramforsmodal.style.display = "none";
+}
+compClose.onclick = function () {
+    compmodal.style.display = "none";
+}
+compCtc.onclick = function () {
+    compmodal.style.display = "none";
+}
+avfallClose.onclick = function () {
+    avfallmodal.style.display = "none";
+}
+avfallCtc.onclick = function () {
+    avfallmodal.style.display = "none";
+}
+sambClose.onclick = function () {
+    sambmodal.style.display = "none";
+}
+sambCtc.onclick = function () {
+    sambmodal.style.display = "none";
+}
+susannaClose.onclick = function () {
+    susannamodal.style.display = "none";
+}
+susannaCtc.onclick = function () {
+    susannamodal.style.display = "none";
+}
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
 //    if (event.target == thesismodal) {
@@ -154,5 +234,24 @@ window.onclick = function (event) {
     }
 	if (event.target == awdmodal) {
         awdmodal.style.display = "none";
-    }
+	}
+
+	if (event.target == sambmodal) {
+	    sambmodal.style.display = "none";
+	}
+	if (event.target == avfallmodal) {
+	    avfallmodal.style.display = "none";
+	}
+	if (event.target == compmodal) {
+	    compmodal.style.display = "none";
+	}
+	if (event.target == kramforsmodal) {
+	    kramforsmodal.style.display = "none";
+	}
+	if (event.target == sprakmodal) {
+	    sprakmodal.style.display = "none";
+	}
+	if (event.target == susannamodal) {
+	    susannamodal.style.display = "none";
+	}
 }
